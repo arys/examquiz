976413aa1b9d660737fa482d36e9ecea4b337792
arys777 <ars-li@mail.ru>
2016-08-31 20:02:01 +0600

    pg

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#new'
+  root 'pages#index'
   # Example of regular route:
   get 'invites' => 'users#invites'
   get 'my_games' => 'users#my_games'
